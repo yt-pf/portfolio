6e5daae9f0d7d5561def7b1228c23f1cbd3c8eb2
user <user@localhost>
2025-09-02 09:48:24 +0900

    PF-2 Create skeleton

---
+++ content/pages/hello.md
@@ -0,0 +1,5 @@
+Title: サンプルページ
+Slug: hello
+save_as: index.html
+
+Hello world!
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,15 @@
-AUTHOR = 'User'
-SITENAME = 'My Portfolio'
+AUTHOR = "User"
+SITENAME = "My Portfolio"
 SITEURL = ""
+
+SITETITLE = "My Portfolio"  # genusテーマで使用
+SITESUBTITLE = ""
 
 PATH = "content"
 
-TIMEZONE = 'Asia/Tokyo'
+TIMEZONE = "Asia/Tokyo"
 
-DEFAULT_LANG = 'Japanese'
+DEFAULT_LANG = "Japanese"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -15,21 +18,18 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
-
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+# THEME = "crowsfoot"
+THEME = "genus"
+
+PAGE_URL = "{category}/{slug}.html"
+PAGE_SAVE_AS = "{category}/{slug}.html"
+
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = False
+
+STATIC_PATHS = ["images", "theme"]
